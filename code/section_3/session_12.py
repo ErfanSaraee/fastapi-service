ca41adc67e9410e045cf7567181ca6f50204cfc9
ErfanSaraee <erfansaraee4102@gmail.com>
2026-08-31 21:28:29 +0330

    feat(session-12): add session 12 code and practice

---
+++ code/section_3/session_12.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI, Query, status, HTTPException
+from fastapi.responses import JSONResponse
+from typing import Optional, Annotated
+
+app = FastAPI()
+
+names_list = [
+    {"id": 1, "name": "Amin"},
+    {"id": 2, "name": "Maryam"},
+    {"id": 3, "name": "Kiana"},
+    {"id": 4, "name": "Soniya"},
+    {"id": 5, "name": "Rasul"},
+    {"id": 6, "name": "Amin"},
+    {"id": 7, "name": "Amin"},
+]
+
+
+# @app.get("/names")
+# def get_names(q: str | None = None):
+#     if q:
+#         return [name for name in names_list if q.lower() in name["name"].lower()]
+#     return names_list
+
+
+# @app.get("/names")
+# def get_names(q: Optional[str] = None):
+#     if q:
+#         return [name for name in names_list if q.lower() in name["name"].lower()]
+#     return names_list
+
+
+@app.get("/names")
+def get_names(q: Annotated[str, Query(deprecated=True, alias="name", description="The name to search for", example="Amin", max_length=50)] = None):
+    if q:
+        return [name for name in names_list if q.lower() in name["name"].lower()]
+    return names_list
+
+
+# @app.get("/names")
+# def get_names(q: str | None = Query(default=None, min_length=50)):
+#     if q:
+#         return [name for name in names_list if q.lower() in name["name"].lower()]
+#     return names_list
+
+
+@app.post("/names", status_code=status.HTTP_201_CREATED)
+def add_name(name: str):
+    names_list.append({"id": len(names_list) + 1, "name": name})
+    return {"message": "Name added successfully"}
+
+
+@app.put("/names/{name_id}", status_code=status.HTTP_200_OK)
+def update_name(name_id: int, name: str):
+    for item in names_list:
+        if item["id"] == name_id:
+            item["name"] = name
+            return {"message": "Name updated successfully"}
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Name not found")
+
+
+@app.delete("/names/{name_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_name(name_id: int):
+    for item in names_list:
+        if item["id"] == name_id:
+            names_list.remove(item)
+            return JSONResponse(content={"message": "Name deleted successfully"}, status_code=status.HTTP_200_OK)
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Name not found")
+
+
+@app.get("/names/{name_id}")
+def get_name(name_id: int):
+    for name in names_list:
+        if name["id"] == name_id:
+            return name
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Name not found")
+
+
+@app.get("/")
+def root():
+    return JSONResponse(content={"message": "Hello World!"}, status_code=status.HTTP_200_OK)
