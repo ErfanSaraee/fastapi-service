f24b1eb1b1450415d2f027ccfc17b03be81e614f
ErfanSaraee <erfansaraee4102@gmail.com>
2026-09-02 20:21:09 +0330

    feat(database-05): add section6/database 05 code and practice

---
+++ tutorials/section_6/database_05_1.py
@@ -0,0 +1,83 @@
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite_05.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+
+# for postgres or other relational databases
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver:5432/db"
+# SQLALCHEMY_DATABASE_URL = "mysql://username:password@localhost/db_name"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}  # only for sqlite
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# create base class for declaring tables
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(30))
+    email = Column(String())
+    password = Column(String())
+    is_active = Column(Boolean, default=True)
+    is_verified = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return f"User(id={self.id},username={self.username},email={self.email})"
+
+
+class Address(Base):
+    __tablename__ = "addresses"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    city = Column(String())
+    state = Column(String())
+    zip_code = Column(String())
+
+    def __repr__(self):
+        return f"Address(id={self.id},user_id={self.user_id},city={self.city},state={self.state},zip_code={self.zip_code})"
+
+
+# to create tables and database
+Base.metadata.create_all(bind=engine)
+
+session = SessionLocal()
+
+# session.add(
+#     User(
+#         username="alibigdeli",
+#         email="alibigdeli@gmail.com",
+#         password="1234"
+#     ))
+# session.commit()
+
+user = session.query(User).filter_by(username="alibigdeli").one_or_none()
+
+# addresses = [
+#     Address(
+#         city="Tehran",
+#         state="Tehran",
+#         zip_code="34000",
+#         user_id=user.id
+#     ), Address(
+#         city="Mashhad",
+#         state="Khorasan-e Razavi",
+#         zip_code="35000",
+#         user_id=user.id
+#     )]
+# session.add_all(addresses)
+# session.commit()
+
+
+addresses = session.query(Address).filter_by(user_id=user.id).all()
+for address in addresses:
+    print(address)
